dafebf4bbfa15bd8e0ac2e14d26f7e779275b1be
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2026-09-10 12:08:31 -0300

    registro segunda clase

--- DOC_2/src/dominio/empleado.py
+++ DOC_2/src/dominio/empleado.py
@@ -5,4 +5,4 @@ class Empleado:
 
     # Asegúrate de que este nombre tenga los guiones bajos completos:
     def mostrar_datos(self) -> str:
-        return f"Empleado: {self.nombre} | Correo: {self.correo}"
+        return f"Empleado: {self.nombre} | Correo: {self.correo}"   
