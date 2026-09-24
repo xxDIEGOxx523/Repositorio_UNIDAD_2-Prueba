6d3b1362e006b7e788fe1597929f1504b7a318e8
69xXDiegoxXx69 <288327205+xxDIEGOxx523@users.noreply.github.com>
2026-09-23 22:57:57 -0300

    Refactor insertar method in EmpleadoDAO class

--- DOC_2/src/dominio/persistencia/empleado_dao.py
+++ DOC_2/src/dominio/persistencia/empleado_dao.py
@@ -1,18 +1,25 @@
-# persistencia/empleado_dao.py
 from persistencia.conexion import abrir_conexion, obtener_motor
 
+
 class EmpleadoDAO:
-     @staticmethod
-     def insertar(empleado):
-         conexion = abrir_conexion()
-         cursor = conexion.cursor()
-         marcador = "?" if obtener_motor() == "sqlite" else "%s"
-         sql = f"""
-             INSERT INTO empleado (nombre, correo)
-             VALUES ({marcador}, {marcador})
+
+    @staticmethod
+    def insertar(empleado):
+        conexion = abrir_conexion()
+        cursor = conexion.cursor()
+
+        marcador = "?" if obtener_motor() == "sqlite" else "%s"
+
+        sql = f"""
+            INSERT INTO empleado (nombre, correo)
+            VALUES ({marcador}, {marcador})
         """
-cursor.execute(sql, (empleado.nombre, empleado.correo))
-empleado.id = cursor.lastrowid
-conexion.commit()
-conexion.close()
-return empleado
+
+        cursor.execute(sql, (empleado.nombre, empleado.correo))
+
+        empleado.id = cursor.lastrowid
+
+        conexion.commit()
+        conexion.close()
+
+        return empleado
